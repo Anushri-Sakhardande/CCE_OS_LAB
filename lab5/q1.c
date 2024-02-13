229ba4224b80df7ad64d526ff8487268878dab10
Anushri Sakhardande <anushri11sakhardande@gmail.com>
2024-02-13 13:36:39 +0530

    lab5

--- lab5/q1.c
+++ lab5/q1.c
@@ -4,8 +4,19 @@
 
 int main()
 {
-	pid_t pid;
-	
-
+        pid_t pid;
+        pid = fork();
+        if(pid ==  0){
+                printf("I am the child \n");
+                printf("PID:%d\n",getpid());
+                printf("PPID:%d\n",getppid());
+                
+        }else if(pid>0){
+                printf("I am the parent\n");
+                printf("PID:%d\n",getpid());
+                printf("PPID:%d\n",getppid());
+        }else{
+                printf("Error");
+        }
+        return 0;
 }
-
